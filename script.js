06a6f1b3da863149738012923761103918cae93c
Lex95 <andrea.lega1995@gmail.com>
2021-05-05 17:16:09 +0200

    aggiunta funzionalità di cancellare i messaggi

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const globalUsersList = [
     },
 ];
 
-const app = new Vue ({
+const app = new Vue({
     el: "#app",
     data: {
         usersList: globalUsersList,
@@ -92,13 +92,15 @@ const app = new Vue ({
         query: "",
     },
     computed: {
+        // fare caso limite di quando non hai messaggi ricevuti per fixare il delete
         lastAccess() {
-            if (!this.usersList[this.activeChat].messages) {
-                return "";
-            };
             const receivedMessages = this.usersList[this.activeChat].messages.filter((msg) => msg.status == "received");
-            const lastMessageDate = receivedMessages[receivedMessages.length - 1];
-            return this.getTimestamp(lastMessageDate)
+            if (receivedMessages.length == 0) {
+                return ""
+            } else {
+                const lastMessageDate = receivedMessages[receivedMessages.length - 1];
+                return this.getTimestamp(lastMessageDate)
+            }
         },
         filteredContacts() {
             return this.usersList.filter(element => element.name.toLowerCase().includes(this.query.toLowerCase()))
@@ -109,31 +111,56 @@ const app = new Vue ({
             this.activeChat = this.usersList.indexOf(contact)
         },
         getTimestamp(message) {
-            const dateFromString = moment(message.date, "DD/MM/YYYY HH:mm:ss");
-            return dateFromString.format("HH:mm")
+            if (message) {
+                const dateFromString = moment(message.date, "DD/MM/YYYY HH:mm:ss");
+                return dateFromString.format("HH:mm")
+            } else return ""
         },
         lastMessage(contact) {
-            if (contact.messages == null) {
-                return "Non ci sono ancora messaggi"
+            if (contact.messages.length == 0) {
+                return ""
             } else {
                 return contact.messages[contact.messages.length - 1]
             }
         },
         sendMessage() {
-            if (this.newMessage != "") {
+            if (this.newMessage.trim() != "") {
                 this.usersList[this.activeChat].messages.push({
                     date: moment().format("DD/MM/YYYY HH:mm:ss"),
                     text: this.newMessage,
                     status: "sent"
                 });
                 this.newMessage = "";
+                const temp = this.activeChat;
                 // funzione che risponde dopo 1s
-                setTimeout(() => 
-                this.usersList[this.activeChat].messages.push({
-                    date: moment().format("DD/MM/YYYY HH:mm:ss"),
-                    text: "ok",
-                    status: "received"
-                }), 1000)
+                setTimeout(() =>
+                    this.usersList[temp].messages.push({
+                        date: moment().format("DD/MM/YYYY HH:mm:ss"),
+                        text: "ok",
+                        status: "received"
+                    }), 1000)
+            }
+        },
+        getSender(message) {
+            if (message.status == "sent") {
+                return "te"
+            } else return this.usersList[this.activeChat].name
+        },
+        messageInfo(message) {
+            alert(
+                `Inviato il ${moment(message.date, "DD/MM/YYYY HH:mm:ss").format("DD/MM/YYYY")} alle ${moment(message.date, "DD/MM/YYYY HH:mm:ss").format("HH:mm:ss")} da ${this.getSender(message)}.
+Contenuto: ${message.text}`
+            )
+        },
+        deleteMessage(message) {
+            const indexToDelete = this.usersList[this.activeChat].messages.indexOf(message);
+            this.usersList[this.activeChat].messages.splice(indexToDelete, 1)
+        },
+        onClickSend(newMessage) {
+            if (newMessage == "") {
+                alert("Microfono non collegato! :(")
+            } else {
+                this.sendMessage()
             }
         }
     }
